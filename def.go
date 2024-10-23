a3387a86ff26d25168e3da4fa261a9036d25520f
xiongjianbo <jianbo.xiong@target-energysolutions.com>
2024-10-23 16:33:05 +0800

    optimizing the codes for the migration

--- def.go
+++ def.go
@@ -7,19 +7,12 @@ const (
 	TypeDown = `down`
 )
 
-type Migrates interface {
-	Run() string
-	Rollback() string
-	ChangeLog() string
-	Version() int64
-}
-
 type migrationsTable struct {
-	Id           int64     `json:"id" gorm:"primaryKey"`
-	Version      int64     `json:"version"`
-	ChangeLog    string    `json:"change_log"`
-	LastMigrate  time.Time `gorm:"autoUpdateTime" json:"last_migrate"`
-	LastRollback time.Time `json:"last_rollback"`
+	Id           int64      `json:"id" gorm:"primaryKey"`
+	Version      int64      `json:"version"`
+	ChangeLog    string     `json:"change_log"`
+	LastMigrate  time.Time  `gorm:"autoUpdateTime" json:"last_migrate"`
+	LastRollback *time.Time `json:"last_rollback"`
 }
 
 func (migrationsTable) TableName() string {
